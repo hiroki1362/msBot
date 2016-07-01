b12bf03e24037f734eae9c0c51c140857a2c955d
Hiroki Fujioka <Hiroki_Fujioka@manager-no-MacBook-Pro-3.local>
2016-07-01 13:44:49 +0900

    jsを修正

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ var restify = require("restify");
 var builder = require("botbuilder");
 
 //BOTの作成
-var bot = new builder.BotConnectorBot({appId: "YourAppId", appSecret: "YourAppSecret"});
+var bot = new builder.BotConnectorBot({appId: process.env.BOTFRAMEWORK_APPID, appSecret: process.env.BOTFRAMEWORK_APPSECRET});
 bot.add("/", function (session) {
-	session.send("よくわかんないけど、こんにちわーー！");
+	session.send("よくわかんないけど、こんにちわーー！あなたは、" + session.message.text + "と言いましたね！？");
 });
 
 var server = restify.createServer();
 server.post("/api/messages", bot.verifyBotFramework(), bot.listen());
+
+server.get(/.*/, restify.serverStatic({
+	"directory": ".",
+	"default": "index.html"
+}));
+
 server.listen(process.env.port || 3789, function () {
 	console.log("%s listening to %s", server.name, server.url);
 });
