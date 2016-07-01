2ee85726e8ab2c1672adadea885e58d8f4f794fb
Hiroki Fujioka <Hiroki_Fujioka@manager-no-MacBook-Pro-3.local>
2016-07-01 16:37:59 +0900

    js再修正

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //初めてのBOT
 var restify = require("restify");
 var builder = require("botbuilder");
+var port = process.env.PORT || 8080;
 
 //BOTの作成
 var bot = new builder.BotConnectorBot({appId: "infodex_DevBot", appSecret: "3dc068bae546425eaa3d5d300eaf1193"});
@@ -12,11 +13,11 @@ bot.add("/", function (session) {
 var server = restify.createServer();
 server.post("/api/messages", bot.verifyBotFramework(), bot.listen());
 
-server.get(/.*/, restify.serveStatic({
-	"directory": ".",
-	"default": "index.html"
-}));
+//server.get(/.*/, restify.serveStatic({
+//	"directory": ".",
+//	"default": "index.html"
+//}));
 
-server.listen(process.env.port || 3978, function () {
+server.listen(port, function () {
 	console.log("%s listening to %s", server.name, server.url);
 });
