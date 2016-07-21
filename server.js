4d7296223765eabb7fcaea4e9ca1b03095a4b612
Hiroki Fujioka <Hiroki_Fujioka@manager-no-MacBook-Pro-3.local>
2016-07-21 17:52:43 +0900

    modify based on docs

--- server.js
+++ server.js
@@ -7,12 +7,37 @@ var botConnectorOptions = {
 	    appSecret: process.env.BOTFRAMEWORK_APPSECRET 
 	};
 var url = "https://api.projectoxford.ai/luis/v1/application?id=" + process.env.LuisAppId +"&subscription-key=" + process.env.LuisSubscriptionKey;
-var luisDialog = new builder.LuisDialog(url);
+
+var nameSetLuis = new builder.LuisRecognizer(url);
+var luisDialog = new builder.LuisRecognizer(url);
+var intents = new builder.IntentDialog({recognizers: [luisDialog]});
+//var luisDialog = new builder.LuisDialog(url);
 
 //BOTの作成
 var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add("/", luisDialog);
+bot.dialog("/", intents);
+intents.matches("what_day", function (session, args) {
+	var date = builder.EntityRecognizer.findEntity(args.entities, "builtin.datetime.date");
+	if (date != undefined && date.resolution != undefined) {
+		var d = new Date(date.resolution.date);
+		var day = "日月火水木金土"[d.getDay()];
+		session.send("その日は「" + day + "曜日」です！" );
+	} else {
+		session.send("ちょっと何言ってるのかわかりません。")
+	}
+})
 
+/*bot.dialog("/", function (session) {
+					 session.Prompts.text(session, "こんにちは！まずは、あなたの名前を入力してくださいね！")
+                	 session.beginDialog("/askName");
+                 });
+
+bot.add("/askName", nameSetLuis);
+
+//bot.add("/startLuis", luisDialog);
+*/
+
+/*
 luisDialog.on("what_day", function (session, args) {
 	console.log('message:');
     console.log(session.message);
@@ -36,7 +61,7 @@ luisDialog.onBegin(function (session, args) {
 luisDialog.onDefault(function (session, args) {
 	session.send("質問を理解できませんでした・・・もう一度、お願いします。")
 });
-
+*/
 
 var server = restify.createServer();
 server.post("/api/messages", bot.verifyBotFramework(), bot.listen());
