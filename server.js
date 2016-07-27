6784b82d755909305fc2abedf5d55dd083b454c1
Hiroki Fujioka <Hiroki_Fujioka@manager-no-MacBook-Pro-3.local>
2016-07-27 21:16:22 +0900

    create waterflow

--- server.js
+++ server.js
@@ -11,8 +11,53 @@ var botConnectorOptions = {
 var bot = new builder.BotConnectorBot(botConnectorOptions);
 
 bot.add("/", function (session) {
-	session.send("こんにちわ！Botの世界！")
+	if (session.userData.name) {
+		session.send("あ、こんにちわ" + session.userData.name + "さん。");
+		session.beginDialog("/greeting")
+	} else {
+		session.send("あ、こんにちわ" + session.userData.name + "さん。");
+		session.beginDialog("/phone");
+	}
 })
+
+bot.add("/greeting", [
+                     function (session) {
+                    	 builder.Prompts.text(session, "こんにちわ！ところで、あなたは何て名前なの？");
+                     },
+                     function (session, result) {
+                    	 session.userData.name = results.response;
+                    	 session.send("こんにちわ！" + session.userData.name + "さん、よろしくね！");
+                    	 session.beginDialog("/greeting")
+                     }
+                      ]);
+
+bot.add("/phone", [
+                   function (session) {
+                	   builder.Prompts.choice("ところで" + session.userData.name + "さん、携帯は何使ってるんでしたっけ？", "iPhone|Android|ガラケー|糸電話|狼煙")
+                   },
+                   function (session, results) {
+                	   var res = results.response.entity;
+                	   if (res == "糸電話" || res == "狼煙") {
+                		   session.send(res + "？はいはい、どうせそういうと思いましたよ・・・真面目に答えてくれないと先に進めないのでちゃんと答えてくださいね。");
+                		   session.endDialog();
+                	   } else {
+                		   session.send("あ、" + res + "を使ってるんですね！");
+                		   session.userData.phone = res;
+                		   session.beginDialo("/company");
+                	   }
+                   }      
+                   ]);
+
+bot.add("/company", [
+                     function (session) {
+                    	 builder.Prompts.texts(session, "さてさて" + session.userData.phone + "使いの" + session.userData.name + "さん、どこにお勤めでしたっけ？");
+                     },
+                     function (session, results) {
+                    	 session.userData.company = results.response;
+                    	 session.send("そうですか！" + session.userData.company + "に勤めてらっしゃるんですね！");
+                    	 session.endDialog();
+                     }
+                     ]);
 
 var server = restify.createServer();
 server.post("/api/messages", bot.verifyBotFramework(), bot.listen());
