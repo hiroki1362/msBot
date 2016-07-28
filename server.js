6c2b91c36ab5debf38c138e24cd6992a82016a7f
Hiroki Fujioka <Hiroki_Fujioka@manager-no-MacBook-Pro-3.local>
2016-07-28 13:24:07 +0900

    create waterfall

--- server.js
+++ server.js
@@ -11,47 +11,43 @@ var botConnectorOptions = {
 var bot = new builder.BotConnectorBot(botConnectorOptions);
 
 bot.add("/", function (session) {
-	if (!session.userData.name) {
-		session.beginDialog("/greeting")
-	} else {
-		session.send("あ、こんにちわ" + session.userData.name + "さん。");
-		session.beginDialog("/greeting/phone");
-	}
+	session.beginDialog("/waterFall");
 })
 
-bot.add("/greeting", [
+bot.add("/waterFall", [
                      function (session) {
-                    	 builder.Prompts.text(session, "こんにちわ！ところで、あなたは何て名前なの？");
+                    	 if (!session.userData.name) {
+                    		 builder.Prompts.text(session, "こんにちわ！ところで、あなたは何て名前なの？");
+                    	 } else {
+                    		 next();
+                    	 }
                      },
                      function (session, results) {
-                    	 session.userData.name = results.response;
-                    	 session.send("こんにちわ！" + session.userData.name + "さん、よろしくね！");
-                    	 session.beginDialog("/greeting/phone")
-                     }
-                      ]);
-
-bot.add("/greeting/phone", [
-                   function (session) {
-                	   builder.Prompts.choice("ところで" + session.userData.name + "さん、携帯は何使ってるんでしたっけ？", "iPhone|Android|ガラケー|糸電話|狼煙")
-                   },
-                   function (session, results) {
-                	   var res = results.response.entity;
-                	   if (res == "糸電話" || res == "狼煙") {
-                		   session.send(res + "？はいはい、どうせそういうと思いましたよ・・・真面目に答えてくれないと先に進めないのでちゃんと答えてくださいね。");
-                		   session.endDialog();
-                	   } else {
-                		   session.send("あ、" + res + "を使ってるんですね！");
-                		   session.userData.phone = res;
-                		   session.beginDialo("/company");
-                	   }
-                   }      
-                   ]);
-
-bot.add("/company", [
+                    	 if (!session.userData.name) {
+                    		 session.userData.name = results.response;
+                        	 session.send("こんにちわ！" + session.userData.name + "さん、よろしくね！");
+                    	 } else {
+                    		 session.send("あ、こんにちわ" + session.userData.name + "さん。");
+                    	 }
+                    	 next();
+                     },
+                     function (session) {
+                    	 builder.Prompts.choice("ところで" + session.userData.name + "さん、携帯は何使ってるんでしたっけ？", "iPhone|Android|ガラケー|糸電話|狼煙");
+                     },
+                     function (session, results) {
+                    	 var res = results.response.entity;
+						 if (res == "糸電話" || res == "狼煙") {
+							session.send(res + "？はいはい、どうせそういうと思いましたよ・・・真面目に答えてくれないと先に進めないのでちゃんと答えてくださいね。");
+							} else {
+								session.send("あ、" + res + "を使ってるんですね！");
+								session.userData.phone = res;
+								next();
+						 }
+                     },
                      function (session) {
                     	 builder.Prompts.texts(session, "さてさて" + session.userData.phone + "使いの" + session.userData.name + "さん、どこにお勤めでしたっけ？");
                      },
-                     function (session, results) {
+                     function (session, results){
                     	 session.userData.company = results.response;
                     	 session.send("そうですか！" + session.userData.company + "に勤めてらっしゃるんですね！");
                     	 session.endDialog();
